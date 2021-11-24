17009b470a73cd8635b6b79a851c87a9ebcf4a0f
Mikko Huotari <dmihu012@edu.xamk.fi>
2021-11-24 11:00:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace Länsi_Saatana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("https://www.lansi-savo.fi/");
+            string theWholePage = document.DocumentNode.OuterHtml;
+            Console.Write(theWholePage);
+        }
+
+
+        HtmlNodeCollection importantRows =
+        document.DocumentNode.SelectNodes("//a[@class='card__link']");
+ 
+        foreach (HtmlNode node in importantRows)
+    {
+        string textValue = node.InnerText;
+        Console.WriteLine(textValue);
+    }
+
+}
+
+}
